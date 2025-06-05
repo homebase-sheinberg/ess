1fe62c02e63ee6f47c51a3813f995e5ccd6a9fd4
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-06-05 11:28:00 -0400

    added code to adjust nhit into params from basic_planko  [author: adit]

--- planko/training/training_loaders.tcl
+++ planko/training/training_loaders.tcl
@@ -1,39 +1,40 @@
-#
-# VARIANTS
-#   planko training
-#
-# DESCRIPTION
-#   loader functions for planko variants
-#
-
 namespace eval planko::training {
     package require planko
-    
+
     proc loaders_init { s } {
-	$s add_method basic_planko { nr nplanks nhit wrong_catcher_alpha params } {
-	    set n_rep $nr
-	    
-	    if { [dg_exists stimdg] } { dg_delete stimdg }
+        $s add_method basic_planko { nr nplanks nhit wrong_catcher_alpha params } {
+            set n_rep $nr
 
-	    
-	    set n_obs [expr [llength $nplanks]*$n_rep]
-	    
-	    set maxx [expr $screen_halfx]
-	    set maxy [expr $screen_halfy]
+            if { [dg_exists stimdg] } {
+                dg_delete stimdg
+            }
 
-	    # this is a set of params to pass into generate_worlds
-	    set p "nplanks $nplanks nhit $nhit $params"
-	    set g [planko::generate_worlds $n_obs $p]
-	    dl_set $g:wrong_catcher_alpha \
-		[dl_repeat [dl_flist $wrong_catcher_alpha] $n_obs]
+            set n_obs [expr {[llength $nplanks] * $n_rep}]
+            set maxx [expr {$screen_halfx}]
+            set maxy [expr {$screen_halfy}]
 
-	    # rename id column to stimtytpe
-	    dg_rename $g:id stimtype 
-	    dl_set $g:remaining [dl_ones $n_obs]
-	    
-	    dg_rename $g stimdg
-	    return $g
-	}
+            # --- Build combined params dict ---
+            # Start with top-level options
+            set p [dict create nplanks $nplanks nhit $nhit]
+
+            # Expand entries from the params list (e.g., jittered block)
+            dict for {k v} [lindex $params 0] {
+                dict set p $k $v
+            }
+
+            # Generate the worlds using the combined param dict
+            set g [planko::generate_worlds $n_obs $p]
+
+            # Apply wrong_catcher_alpha
+            dl_set $g:wrong_catcher_alpha \
+                [dl_repeat [dl_flist $wrong_catcher_alpha] $n_obs]
+
+            # Rename and finalize
+            dg_rename $g:id stimtype
+            dl_set $g:remaining [dl_ones $n_obs]
+            dg_rename $g stimdg
+
+            return $g
+        }
     }
 }
-
