2d96c33204ba2c9a62c7fddd15b0de8f5809560f
David Sheinberg <David_Sheinberg@brown.edu>
2025-06-18 18:00:45 -0400

    - 2025-06-18 18:00:45

--- planko/training/training_stim.tcl
+++ planko/training/training_stim.tcl
@@ -1,10 +1,11 @@
 # NAME
-#   circles_stim.tcl
+#   planko_stim.tcl
 #
 # DESCRIPTION
-#   search with circles
+#   planko task stim code
 #
 # REQUIRES
+#   box2d 
 #   polygon
 #   metagroup
 #
@@ -21,16 +22,16 @@ package require box2d
 proc check_contacts { w } {
     if { [setObjProp $w complete] } { return }
     if { [Box2D_getContactBeginEventCount $w] > 0 } {
-	set contacts [Box2D_getContactBeginEvents $w]
-	foreach c $contacts {
-	    if { [lsearch $c catchl_b] >= 0 } {
-		qpcs::dsSet $::dservhost planko/complete left
-		setObjProp $w complete 1
-	    } elseif { [lsearch $c catchr_b] >= 0 } {
-		qpcs::dsSet $::dservhost planko/complete right
-		setObjProp $w complete 1
-	    }
-	}
+        set contacts [Box2D_getContactBeginEvents $w]
+        foreach c $contacts {
+            if { [lsearch $c catchl_b] >= 0 } {
+                qpcs::dsSet $::dservhost planko/complete left
+                setObjProp $w complete 1
+            } elseif { [lsearch $c catchr_b] >= 0 } {
+                qpcs::dsSet $::dservhost planko/complete right
+                setObjProp $w complete 1
+            }
+        }
     }
 }
 
@@ -39,12 +40,12 @@ proc check_contacts { w } {
 ##############################################################
 
 proc make_stims { trial } {
-    
-    resetObjList		 ;# unload existing objects
-    glistInit 1			 ;# initialize stimuli
+
+    resetObjList ;# unload existing objects
+    glistInit 1 ;# initialize stimuli
 
     set dg stimdg
-    
+
     set bworld [Box2D]
     glistAddObject $bworld 0
     setObjProp $bworld complete 0
@@ -52,40 +53,40 @@ proc make_stims { trial } {
     set ::left_catcher {}
     set ::right_catcher {}
     set ::planks {}
-    
+
     set n [dl_length $dg:name:$trial]
 
     # get side and show_only_correct_side flag for this trial
     foreach v "side wrong_catcher_alpha" {
-	set $v [dl_get $dg:$v $trial]
+        set $v [dl_get $dg:$v $trial]
     }
-    
+
     for { set i 0 } { $i < $n } { incr i } {
-	foreach v "name shape type tx ty sx sy angle restitution" {
-	    set $v [dl_get $dg:$v:$trial $i]
-	}
-	if { $shape == "Box" } {
-	    if { $side == "0" } { set wrong_catcher catchr_* } { set wrong_catcher catchl_* }
-	    if { [string match $wrong_catcher $name] } { set alpha $wrong_catcher_alpha } { set alpha 1.0 }
-	    set body [create_box $bworld $name $type $tx $ty $sx $sy $angle [list 9. 9. 9. $alpha ]]
-	} elseif { $shape == "Circle" } {
-	    set body [create_circle $bworld $name $type $tx $ty $sx $angle { 0 1 1 1 }]
-	}
-	Box2D_setRestitution $bworld [setObjProp $body body] $restitution
-	
-	glistAddObject $body 0
+        foreach v "name shape type tx ty sx sy angle restitution" {
+            set $v [dl_get $dg:$v:$trial $i]
+        }
+        if { $shape == "Box" } {
+            if { $side == "0" } { set wrong_catcher catchr_* } { set wrong_catcher catchl_* }
+            if { [string match $wrong_catcher $name] } { set alpha $wrong_catcher_alpha } { set alpha 1.0 }
+            set body [create_box $bworld $name $type $tx $ty $sx $sy $angle [list 9. 9. 9. $alpha ]]
+        } elseif { $shape == "Circle" } {
+            set body [create_circle $bworld $name $type $tx $ty $sx $angle { 0 1 1 1 }]
+        }
+        Box2D_setRestitution $bworld [setObjProp $body body] $restitution
 
-	# track this so we can set in motion
-	if { $name == "ball" } { set ::ball $body }
+        glistAddObject $body 0
 
-	# track catcher bodies so we can give feedback
-	if { [string match catchl* $name] } { lappend ::left_catcher $body }
-	if { [string match catchr* $name] } { lappend ::right_catcher $body }
-	if { [string match plank* $name] } { lappend ::planks $body }
+        # track this so we can set in motion
+        if { $name == "ball" } { set ::ball $body }
+
+        # track catcher bodies so we can give feedback
+        if { [string match catchl* $name] } { lappend ::left_catcher $body }
+        if { [string match catchr* $name] } { lappend ::right_catcher $body }
+        if { [string match plank* $name] } { lappend ::planks $body }
     }
 
     addPostScript $bworld [list check_contacts $bworld]
-    
+
     glistSetDynamic 0 1
     return $bworld
 }
@@ -106,12 +107,12 @@ proc create_box { bworld name type tx ty sx sy { angle 0 } { color { 1 1 1 } } }
     set degrees [expr $angle*(180./$::pi)]
     set m [dl_tcllist [mat4_createTranslationAngle $tx $ty $degrees]]
     setObjMatrix $box {*}$m
-    
+
     # link the box2d box to the polygon
     Box2D_linkObj $bworld $body $box
     setObjProp $box body $body
     setObjProp $box bworld $bworld
-    
+
     return $box
 }
 
@@ -131,12 +132,12 @@ proc create_circle { bworld name type tx ty radius { angle 0 } { color { 1 1 1 }
     set degrees [expr $angle*(180./$::pi)]
     set m [dl_tcllist [mat4_createTranslationAngle $tx $ty $degrees]]
     setObjMatrix $circ {*}$m
-    
+
     # link the box2d circle to the polygon
     Box2D_linkObj $bworld $body $circ
     setObjProp $circ body $body
     setObjProp $circ bworld $bworld
-    
+
     return $circ
 }
 
@@ -163,19 +164,19 @@ proc show_response { resp } {
     set body [setObjProp $::ball body]
     Box2D_setBodyType $::world $body 2
     if { $resp == 0 } { set c $::left_catcher } { set c $::right_catcher }
-    set color     "0.7 0.7 0.7"
+    set color "0.7 0.7 0.7"
     foreach p $c {
-	polycolor $p {*}$color
+        polycolor $p {*}$color
     }
 }
 
 proc show_feedback { resp correct } {
     if { $resp == 0 } { set c $::left_catcher } { set c $::right_catcher }
-    set green   "0.2 .9 .3"
-    set red     "1.0 .2 .2"
+    set green "0.2 .9 .3"
+    set red "1.0 .2 .2"
     foreach p $c {
-	if { $correct } { set color $green } { set color $red }
-	polycolor $p {*}$color
+        if { $correct } { set color $green } { set color $red }
+        polycolor $p {*}$color
     }
 }
 
@@ -192,14 +193,14 @@ proc stimoff {} {
 
 proc plankson {} {
     foreach p $::planks {
-	setVisible $p 1
+        setVisible $p 1
     }
     redraw
 }
 
 proc planksoff {} {
     foreach p $::planks {
-	setVisible $p 0
+        setVisible $p 0
     }
     redraw
 }
