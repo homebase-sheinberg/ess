82d8f6f5495db0b1720076f373675d7a6faa4862
David Sheinberg <David_Sheinberg@brown.edu>
2025-06-23 12:16:51 -0400

    commit 2025-06-23 12:16:51

--- emcalib/9point/9point_stim.tcl
+++ emcalib/9point/9point_stim.tcl
@@ -18,33 +18,49 @@ proc nexttrial { id } {
     resetObjList
 
     set fix_color ".7 .7 .1"
-    
+
     foreach p "fix_targ_x fix_targ_y fix_targ_r" {
-	set $p [dl_get stimdg:$p $id]
+        set $p [dl_get stimdg:$p $id]
     }
-    
+
     # load the initial fixation
+    set mg [metagroup]
     set obj [polygon]
     polycirc $obj 1
     polycolor $obj {*}$fix_color
     translateObj $obj $fix_targ_x $fix_targ_y
-    scaleObj $obj [expr 2*$fix_targ_r]; # diameter is 2r
-    glistAddObject $obj 0
+    scaleObj $obj [expr {2*$fix_targ_r}]; # diameter is 2r
+    set center [polygon]
+    polycirc $center 1
+    polycolor $center 0 0 0
+    translateObj $center $fix_targ_x $fix_targ_y
+    scaleObj $center [expr {0.3*2*$fix_targ_r}]; # diameter is 2r
+    metagroupAdd $mg $obj
+    metagroupAdd $mg $center
+    glistAddObject $mg 0
 
     foreach p "jump_targ_x jump_targ_y jump_targ_r" {
-	set $p [dl_get stimdg:$p $id]
+        set $p [dl_get stimdg:$p $id]
     }
-    
+
     # load the jump
+    set mg [metagroup]
     set obj [polygon]
     polycirc $obj 1
     polycolor $obj {*}$fix_color
     translateObj $obj $jump_targ_x $jump_targ_y
-    scaleObj $obj [expr 2*$jump_targ_r]; # diameter is 2r
-    glistAddObject $obj 1  
+    scaleObj $obj [expr {2*$jump_targ_r}]; # diameter is 2r
+    set center [polygon]
+    polycirc $center 1
+    polycolor $center 0 0 0
+    translateObj $center $jump_targ_x $jump_targ_y
+    scaleObj $center [expr {0.3*2*$jump_targ_r}]; # diameter is 2r
+    metagroupAdd $mg $obj
+    metagroupAdd $mg $center
+    glistAddObject $mg 1
 
 }
-    
+
 proc fixon {} {
     glistSetCurGroup 0
     glistSetVisible 1
