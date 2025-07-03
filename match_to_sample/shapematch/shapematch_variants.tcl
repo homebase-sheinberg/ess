ac9134c00a457dfddfbbd13fe9f7cc870f5c3274
homebase-sheinberg <ryan-bot@company.com>
2025-07-03 18:13:03 +0000

    tabbing

--- match_to_sample/shapematch/shapematch_variants.tcl
+++ match_to_sample/shapematch/shapematch_variants.tcl
@@ -12,20 +12,20 @@ namespace eval match_to_sample::shapematch {
 	    description "match with single pair of shapes"
 	    loader_proc setup_trials
 	    loader_options {
-		n_rep { 50 100 200 400 800}
-		targ_scale 3
-		shape_choices single_pair
-		shape_params {}
+			n_rep { 50 100 200 400 800}
+			targ_scale 3
+			shape_choices single_pair
+			shape_params {}
 	    }
 	}
 	single_pair_arbitrary {
 	    description "match with single pair of arbitrarily associated shapes"
 	    loader_proc setup_trials
 	    loader_options {
-		n_rep { 50 100 200 400 800}
-		targ_scale 3
-		shape_choices single_pair_arbitrary
-		shape_params {}
+			n_rep { 50 100 200 400 800}
+			targ_scale 3
+			shape_choices single_pair_arbitrary
+			shape_params {}
 	    }
 	}
     }
