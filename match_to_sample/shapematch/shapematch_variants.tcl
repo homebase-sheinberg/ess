eb217bddd9ad3dcba430181b6fa1ac8299620beb
homebase-sheinberg <ryan-bot@company.com>
2025-07-03 19:28:09 +0000

    re-jigger formatting

--- match_to_sample/shapematch/shapematch_variants.tcl
+++ match_to_sample/shapematch/shapematch_variants.tcl
@@ -8,28 +8,28 @@
 
 namespace eval match_to_sample::shapematch {
     variable variants {
-	single_pair     {
-	    description "match with single pair of shapes"
-	    loader_proc setup_trials
-	    loader_options {
-		n_rep { 50 100 200 400 800}
-		targ_scale 3
-		shape_choices single_pair
-		shape_params {}
-		show_feedback {0 1}
-	    }
-	}
-	single_pair_arbitrary {
-	    description "match with single pair of arbitrarily associated shapes"
-	    loader_proc setup_trials
-	    loader_options {
-		n_rep { 50 100 200 400 800}
-		targ_scale 3
-		shape_choices single_pair_arbitrary
-		shape_params {}
-		show_feedback {0 1}
-	    }
-	}
+		single_pair     {
+			description "match with single pair of shapes"
+			loader_proc setup_trials
+			loader_options {
+				n_rep { 50 100 200 400 800}
+				targ_scale 3
+				shape_choices single_pair
+				show_feedback {0 1}
+				shape_params {}			
+			}
+		}
+		single_pair_arbitrary {
+			description "match with single pair of arbitrarily associated shapes"
+			loader_proc setup_trials
+			loader_options {
+				n_rep { 50 100 200 400 800}
+				targ_scale 3
+				shape_choices single_pair_arbitrary
+				show_feedback {0 1}
+				shape_params {}
+			}
+		}
     }
 }
 
