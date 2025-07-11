dc2728dd6141d45c9c94fece76584c0c19fa7d61
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-07-11 13:08:46 -0400

    added storage of jittered simulation info into basic_planko loaders [author: adit]

--- planko/training/training_loaders.tcl
+++ planko/training/training_loaders.tcl
@@ -24,7 +24,21 @@ namespace eval planko::training {
             # this is a set of params to pass into generate_worlds
             set p "nplanks $nplanks $params"
             set g [planko::generate_worlds $n_obs $p]
+            
+            # Run jitter simulations
+            set njitter 10
+            set jittered [world_jitter $g $njitter 0 [dl_length $g:id]]
 
+            # Embed jittered data with 'j_' prefixes
+            dl_set $g:j_jx               $jittered:jx
+            dl_set $g:j_jy               $jittered:jy
+            dl_set $g:j_angle            $jittered:angle
+            dl_set $g:j_jt               $jittered:t
+            dl_set $g:j_jx_ball          $jittered:x
+            dl_set $g:j_jy_ball          $jittered:y
+            dl_set $g:j_distance         $jittered:distance
+            dl_set $g:j_distance_matrix  $jittered:distance_matrix
+    
             dl_set $g:wrong_catcher_alpha \
                 [dl_repeat [dl_flist $wrong_catcher_alpha] $n_obs]
 
