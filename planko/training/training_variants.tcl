973bb5686b136cea00622abe01c00517fae03606
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-07-14 12:31:54 -0400

    Update planko/training/training_variants.tcl [author: adit]

--- planko/training/training_variants.tcl
+++ planko/training/training_variants.tcl
@@ -64,7 +64,7 @@ namespace eval planko::training {
             }
         }
 
-        super_monkey {
+        superr_monkey {
             description "jitter X ball start, vary hitplanks, select stim_dur"
             loader_proc basic_planko
             loader_options {
