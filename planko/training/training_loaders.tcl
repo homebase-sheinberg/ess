27c47717c1a128311b9237e5efc36ac0e93fd320
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-07-15 11:52:41 -0400

    Update planko/training/training_loaders.tcl [author: adit]

--- planko/training/training_loaders.tcl
+++ planko/training/training_loaders.tcl
@@ -3,73 +3,97 @@
 #   planko training
 #
 # DESCRIPTION
-#   loader functions for planko variants
+#   variant dictionary for planko::training
 #
 
 namespace eval planko::training {
-    package require planko
-    
-    proc loaders_init { s } {
-        $s add_method basic_planko { nr nplanks wrong_catcher_alpha params } {
-            set n_rep $nr
-
-            if { [dg_exists stimdg] } {
-                dg_delete stimdg
+    variable variants {
+        single {
+            description "one plank"
+            loader_proc basic_planko
+            loader_options {
+                nr { 100 200 300 }
+                nplanks { 1 }
+                wrong_catcher_alpha 1
+                params { { defaults {} } }
             }
+            init {
+                rmtSend "setBackground 0 0 10"
+            }
+            deinit {}
+        }
 
-            set n_obs [expr {[llength $nplanks] * $n_rep}]
-            set maxx [expr {$screen_halfx}]
-            set maxy [expr {$screen_halfy}]
-        
-            
-            # this is a set of params to pass into generate_worlds
-            set p "nplanks $nplanks $params"
-            set g [planko::generate_worlds $n_obs $p]
-            
+        jitter {
+            description "jitter ball start"
+            loader_proc basic_planko
+            loader_options {
+                nr { 50 100 200 }
+                nplanks { 1 }
+                wrong_catcher_alpha { 1 0.5 }
+                params { 
+                    { jittered { ball_jitter_x 8 ball_start_y 5 ball_jitter_y 1 } } 
+                }
+            }
+        }
 
-            dl_set $g:wrong_catcher_alpha \
-                [dl_repeat [dl_flist $wrong_catcher_alpha] $n_obs]
+        zero_one {
+            description "hit zero or one plank"
+            loader_proc basic_planko
+            loader_options {
+                nr { 50 100 200 400 800 }
+                nplanks { 1 }
+                wrong_catcher_alpha { 1.0 0.98 0.95 0.9 0.8 0.7 }
+                params { 
+                    { jittered { ball_jitter_x 10 ball_start_y 0 ball_jitter_y 3 minplanks 0 } } 
+                }
+            }
+        }
 
-            # rename id column to stimtype
-            dg_rename $g:id stimtype
-            dl_set $g:remaining [dl_ones $n_obs]
+        two_plus {
+            description "show 2+ planks, hit 1+ plank"
+            loader_proc basic_planko
+            loader_options {
+                nr { 50 100 200 400 800 }
+                nplanks { 2 3 4 }
+                wrong_catcher_alpha { 1.0 0.98 0.95 0.9 0.8 0.7 }
+                params { 
+                    { jittered   { ball_jitter_x 10 ball_start_y 0 ball_jitter_y 3 minplanks 1 } }
+                    { higher     { ball_jitter_x 10 ball_start_y 5 ball_jitter_y 3 minplanks 1 } }
+                    { two_plank  { ball_jitter_x 10 ball_start_y 5 ball_jitter_y 3 minplanks 2 } }
+                }
+            }
+        }
 
-            dg_rename $g stimdg
-            return $g
+        super_monkey {
+            description "jitter X ball start, vary hitplanks, select stim_dur"
+            loader_proc basic_planko
+            loader_options {
+                nr { 10 20 30 40 50 60 70 80 90 100 }
+                nplanks { 10 }
+                wrong_catcher_alpha { 1.0 }
+                params {
+                    { jittered_hit1 { ball_jitter_x 6 ball_start_y 9 ball_jitter_y 0 minplanks 1} }
+                    { jittered_hit2 { ball_jitter_x 6 ball_start_y 9 ball_jitter_y 0 minplanks 2} }
+                    { jittered_hit3 { ball_jitter_x 6 ball_start_y 9 ball_jitter_y 0 minplanks 3} }
+                    { jittered_hit4 { ball_jitter_x 6 ball_start_y 9 ball_jitter_y 0 minplanks 4} }
+                }
+            }
         }
         
         
-        $s add_method random_hit_loader { nr nplanks wrong_catcher_alpha params } {
-            set n_rep $nr
-        
-            if { [dg_exists stimdg] } {
-                dg_delete stimdg
+        monkey_mixer {
+            description "random trial-wise minplanks"
+            loader_proc random_hit_loader
+            loader_options {
+                nr { 50 100 }
+                nplanks { 10 }
+                wrong_catcher_alpha { 1.0 }
+                params {
+                    { default { ball_jitter_x 6 ball_start_y 9 ball_jitter_y 0 } }
+                }
             }
-        
-            set n_obs [expr {[llength $nplanks] * $n_rep}]
-            set maxx [expr {$screen_halfx}]
-            set maxy [expr {$screen_halfy}]
-        
-            # Build randomized minplanks list
-            set randlist [dl_ilist]
-            for {set i 0} {$i < $n_obs} {incr i} {
-                dl_append $randlist [expr {1 + int(rand() * 3)}]
-            }
-        
-            # Append minplanks randlist to existing params
-            append params " minplanks $randlist"
-        
-            set p "nplanks $nplanks $params"
-            set g [planko::generate_worlds $n_obs $p]
-        
-            dl_set $g:wrong_catcher_alpha \
-                [dl_repeat [dl_flist $wrong_catcher_alpha] $n_obs]
-        
-            dg_rename $g:id stimtype
-            dl_set $g:remaining [dl_ones $n_obs]
-            dg_rename $g stimdg
-            return $g
         }
+
 
 
     }
