6e551a9a4f6ac69af480592090a1b5da8a8ab517
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-07-31 13:02:15 -0400

    Copy 1 file(s) from ryan [author: admin]

--- match_to_sample/colormatch/colormatch_variants.tcl
+++ match_to_sample/colormatch/colormatch_variants.tcl
@@ -14,7 +14,7 @@ namespace eval match_to_sample::colormatch {
             loader_options {
                 n_rep { 50 100 200 400 800}
                 targ_scale 1.5
-                transparency { {off 0.0} {0.2 0.2} {mixed {0.0 0.2}} {full 1.0} }
+                transparency { {off 0.0} {0.05 0.05} {0.1 0.1} {0.2 0.2} {mixed {0.0 0.2}} {full 1.0} }
             }
         }
         easy {
