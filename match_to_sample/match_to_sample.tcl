53c31e299aa73d59574c188f93581d596a1dc15f
David Sheinberg <David_Sheinberg@brown.edu>
2025-09-05 01:20:00 -0400

    commit 2025-09-05 01:20:00

--- match_to_sample/match_to_sample.tcl
+++ match_to_sample/match_to_sample.tcl
@@ -16,11 +16,11 @@ namespace eval match_to_sample {
 	#                          System Parameters                         #
 	######################################################################
 	
-	$sys add_param start_delay         0      time int
-	$sys add_param interblock_time  1000      time int
-	$sys add_param sample_pre_time     0      time int
+	$sys add_param start_delay          0      time int
+	$sys add_param interblock_time   1000      time int
+	$sys add_param sample_pre_time      0      time int
 	$sys add_param sample_time      15000      time int
-	$sys add_param delay_time       1000      time int
+	$sys add_param delay_time        1000      time int
 	
 	$sys add_param response_timeout 10000      time int
 	
@@ -320,4 +320,6 @@ namespace eval match_to_sample {
 	return $sys
     }
 }
+
+
 
