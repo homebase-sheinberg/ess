48624d88da8df2060004c9ea464864e754054572
homebase-sheinberg <ryan-bot@company.com>
2025-10-03 15:39:06 +0000

    trying to get it to show the arbtrary option in my webpage (it already works outside that)

--- match_to_sample/shapematch/shapematch_variants.tcl
+++ match_to_sample/shapematch/shapematch_variants.tcl
@@ -8,16 +8,16 @@
 
 namespace eval match_to_sample::shapematch {
     variable variants {
-	single_pair     {
-	    description "match with single pair of shapes"
-	    loader_proc setup_trials
-	    loader_options {
-		n_rep { 50 100 200 400 800}
-		targ_scale 3
-			shape_choices { single_pair arbitrary}
-		shape_params {}
-	    }
-	}
+		single_pair     {
+			description "match with single pair of shapes"
+			loader_proc setup_trials
+			loader_options {
+				n_rep { 50 100 200 400 800}
+				targ_scale 3
+				shape_choices { single_pair arbitrary }
+				shape_params {}
+			}
+		}
     }
 }
 
