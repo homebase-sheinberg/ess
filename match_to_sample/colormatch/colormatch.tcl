f18d7a45a22df8609d1bd7c7b13d91316bf0ea4f
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-11-11 13:24:46 -0500

    fix abort correct choice label again [author: defne]

--- match_to_sample/colormatch/colormatch.tcl
+++ match_to_sample/colormatch/colormatch.tcl
@@ -218,8 +218,8 @@ namespace eval match_to_sample::colormatch {
                 return 1
             } elseif { [::ess::touch_in_win 2] } {
                 if { $abort_on_bad_touch } {
-                    set correct 1
-                    return 0
+                    set correct 0
+                    return 1
                 }
             } else {
                 return -1
