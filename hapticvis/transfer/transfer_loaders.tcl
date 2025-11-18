26abb9ad78a21a5d16f7db0569aadc1986e50bc7
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-11-18 13:44:11 -0500

    Update hapticvis/transfer/transfer_loaders.tcl [author: ayan]

--- hapticvis/transfer/transfer_loaders.tcl
+++ hapticvis/transfer/transfer_loaders.tcl
@@ -52,7 +52,7 @@ namespace eval hapticvis::transfer {
 
             # choose random index to select dist center randomly
             # dl_local dist_id [dl_irand $n [expr $n_choices-1]]
-            dl_local dist_id [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [expr $n_choices-1]] [expr {$n_choices*2}]]
+            dl_local dist_id [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]] [expr {$n_choices*2}]]
             dl_local dist_choice_center [dl_choose $dist_choice_centers [dl_pack $dist_id]]
 
             # for half presentations show actual location for other half not
@@ -90,7 +90,7 @@ namespace eval hapticvis::transfer {
 
             # choose random index to select dist center randomly
             # dl_local dist_id [dl_irand $n [expr $n_choices-1]]
-            dl_local dist_id [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [expr $n_choices-1]] [expr {$n_choices*2}]]
+            dl_local dist_id [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]] [expr {$n_choices*2}]]
             # print "Test Distractor Locations: [dl_tcllist [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [expr $n_choices-1]] [expr {$n_choices*2}]]]"
             print "Current Distractor Locations: [dl_tcllist $dist_id]"
             dl_local dist_choice_center [dl_choose $dist_choice_centers [dl_pack $dist_id]]
