7b0c03e36f20bab4dfbf2f6f725fc8cbdfa5dec9
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-11-18 14:44:03 -0500

    Update hapticvis/transfer/transfer_loaders.tcl [author: ayan]

--- hapticvis/transfer/transfer_loaders.tcl
+++ hapticvis/transfer/transfer_loaders.tcl
@@ -84,17 +84,17 @@ namespace eval hapticvis::transfer {
             dl_local choice_loc_ids [dl_fromto 0 [dl_repeat $n_choices $n]]
             dl_local dist_locs [dl_select $choice_loc_ids [dl_noteq $choice_loc_ids $target_loc]]
             # print "Distractor Locations: [dl_tcllist $dist_locs]"
-            print "Number of Trials: $n"
+            # print "Number of Trials: $n"
             
             # pull out all non-target choice locations
             dl_local dist_choice_centers [dl_choose stimdg:choice_centers $dist_locs]
 
             # choose random index to select dist center randomly
             # dl_local dist_id [dl_irand $n [expr $n_choices-1]]
-            dl_local dist_id [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]] [expr {$n_choices*2}]]
-            print "Length no replication: [dl_length [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]]]"
+            dl_local dist_id [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]] [expr {$n / [dl_length [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]]]}]]
+            # print "Length no replication: [dl_length [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]]]"
             # print "Test Distractor Locations: [dl_tcllist [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [expr $n_choices-1]] [expr {$n_choices*2}]]]"
-            # print "Current Distractor Locations: [dl_tcllist $dist_id]"
+            print "Current Distractor Locations: [dl_tcllist $dist_id]"
             dl_local dist_choice_center [dl_choose $dist_choice_centers [dl_pack $dist_id]]
 
             # for half presentations show actual location for other half not
