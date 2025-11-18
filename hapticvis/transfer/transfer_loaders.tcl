3aee2ae5a056c61ed0dd907c30657f0c382a72cd
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-11-18 15:11:51 -0500

    Update hapticvis/transfer/transfer_loaders.tcl [author: ayan]

--- hapticvis/transfer/transfer_loaders.tcl
+++ hapticvis/transfer/transfer_loaders.tcl
@@ -92,7 +92,7 @@ namespace eval hapticvis::transfer {
             # choose random index to select dist center randomly
             # dl_local dist_id [dl_irand $n [expr $n_choices-1]]
             dl_local dist_id [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]] [expr {$n / [dl_length [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]]]}]]
-            # print "Length no replication: [dl_length [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]]]"
+            print "Length no replication: [dl_length [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [llength $rotations]]]"
             # print "Test Distractor Locations: [dl_tcllist [dl_replicate [dl_repeat [dl_fromto 0 [expr $n_choices-1]] [expr $n_choices-1]] [expr {$n_choices*2}]]]"
             print "Current Distractor Locations: [dl_tcllist $dist_id]"
             dl_local dist_choice_center [dl_choose $dist_choice_centers [dl_pack $dist_id]]
