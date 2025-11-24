649a956f9234412fa8b753994fa761ba8b7fbc49
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-11-24 13:42:11 -0500

    Copy 1 file(s) from main [author: ayan]

--- hapticvis/identify/identify.tcl
+++ hapticvis/identify/identify.tcl
@@ -48,15 +48,7 @@ namespace eval hapticvis::identify {
             # register to listen for haptic events
             ::haptic::init
 
-            soundReset
-            soundSetVoice 81 0 0
-            soundSetVoice 57 17 1
-            soundSetVoice 60 0 2
-            soundSetVoice 42 0 3
-            soundSetVoice 21 0 4
-            soundSetVoice 8 0 5
-            soundSetVoice 113 100 6
-            foreach i "0 1 2 3 4 5 6" { soundVolume 127 $i }
+	    ::ess::sound_init
         }
 
         $s set_protocol_deinit_callback {
@@ -170,7 +162,7 @@ namespace eval hapticvis::identify {
         }
 
         $s add_method stim_on {} {
-            soundPlay 1 70 200
+            ::ess::sound_play 1 70 200
             rmtSend "!stim_on"
         }
 
@@ -274,17 +266,17 @@ namespace eval hapticvis::identify {
         }
 
         $s add_method reward {} {
-            soundPlay 3 70 70
+            ::ess::sound_play 3 70 70
             ::ess::reward $juice_ml
             ::ess::evt_put REWARD MICROLITERS [now] [expr {int($juice_ml*1000)}]
         }
 
         $s add_method noreward {} {
-            soundPlay 4 90 300
+            ::ess::sound_play 4 90 300
         }
 
         $s add_method finale {} {
-            soundPlay 6 60 400
+            ::ess::sound_play 6 60 400
         }
 
         $s add_method response_correct {} {
