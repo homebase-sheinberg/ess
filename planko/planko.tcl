4d52f66d75fda72c20a48c1050479337eca05f27
homebase-sheinberg <homebase.sheinberg@gmail.com>
2026-01-06 11:26:53 -0500

    Revert branch 'brent' to state of commit b204165 [author: brent]

--- planko/planko.tcl
+++ planko/planko.tcl
@@ -27,7 +27,6 @@ namespace eval planko {
         $sys add_param response_timeout 25000 time int
         $sys add_param max_feedback_time 8000 time int
         $sys add_param post_feedback_time 1000 time int
-        $sys add_param incorrect_delay_time 0 time int
 
         $sys add_param stimup_time -1 time int
 
@@ -324,15 +323,10 @@ namespace eval planko {
         #
         $sys add_action post_trial {
             ::ess::save_trial_info $correct $rt $stimtype
-            if { !$correct } {
-                timerTick $incorrect_delay_time
-            }
         }
 
         $sys add_transition post_trial {
-            if { [timerExpired] } {
-                return finish
-            }
+            return finish
         }
 
         #
