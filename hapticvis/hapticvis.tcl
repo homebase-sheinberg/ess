85038587dda43a2686affff1cd846f9835ace6e7
David Sheinberg <David_Sheinberg@brown.edu>
2026-02-11 12:39:59 -0500

    don't call choices/cue off on response

--- hapticvis/hapticvis.tcl
+++ hapticvis/hapticvis.tcl
@@ -336,12 +336,10 @@ namespace eval hapticvis {
                 set sample_up -1
             }
             if { $choices_up == 1 } {
-                my choices_off
                 ::ess::evt_put CHOICES OFF [now]
                 set choices_up -1
             }
             if { $cue_up == 1 } {
-                my cue_off
                 ::ess::evt_put CUE OFF [now]
                 set cue_up -1
             }
@@ -522,4 +520,5 @@ namespace eval hapticvis {
         return $sys
     }
 }
+
 
